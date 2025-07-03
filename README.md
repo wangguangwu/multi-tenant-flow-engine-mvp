2fd1a5105b4876350ca0298a296d0cc1e527d4de
wangguangwu <wangguangwu@pukanghealth.com>
2025-07-03 10:04:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# multi-tenant-flow-engine-mvp
---
+++ src/main/java/com/wangguangwu/multitenantflowenginemvp/MultiTenantFlowEngineMvpApplication.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.multitenantflowenginemvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantFlowEngineMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantFlowEngineMvpApplication.class, args);
+	}
+
+}
